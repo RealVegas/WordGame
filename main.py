c56e830986a0800e6b60be6b419eaf644118722b
Вадим Голов <grafit-design@inbox.ru>
2024-10-22 22:39:06 +0500

    Закончил с парсингом HTML.

--- main.py
+++ main.py
@@ -1,67 +1,89 @@
+import re
 import requests
+from random import choice
 from bs4 import BeautifulSoup
 
 
-def get_html() -> list[str]:
+def clean_string(line: str) -> str:
+    text: str = re.sub(r'^[^а-яА-ЯёЁ]+', '', line).strip()
 
+    if text == '':
+        return ''
+
+    text: str = ' '.join(text.split())
+
+    forbidden_words: list[str] = ['антилокус', 'Антилокус', 'локус', 'Локус', '()']
+
+    for item in forbidden_words:
+        text = text.replace(item, '')
+
+    closing_quote: int = text.find('»', 1)
+    if closing_quote != -1:
+        text: str = text[:closing_quote + 1]
+
+    text = text[0].upper() + text[1:]
+
+    return text
+
+
+def clean_words(word: str) -> str:
+    clean = word.replace('«', '')
+    clean = word.replace('»', '')
+
+    clean = re.sub(r'[^а-яА-ЯёЁ]+$', '', word)
+
+    open_bracket: int = clean.find('(', 1)
+    if open_bracket != -1:
+        clean: str = clean[:open_bracket]
+
+    comma_pos: int = clean.find(',', 1)
+    if comma_pos != -1:
+        clean: str = clean[:comma_pos]
+
+    slash_pos: int = clean.find('/', 1)
+    if slash_pos != -1:
+        clean: str = clean[:slash_pos]
+
+    clean = clean[0].upper() + clean[1:]
+
+    clean = clean.strip()
+
+    return clean
+
+
+def get_html() -> list[str]:
     slovar_url: str = 'https://slovar.kakras.ru'
     txt_html: str = requests.get(slovar_url).text
 
     raw_html: list[str] = txt_html.split('\r\n')
-    prep_html: list[str] = [item.strip() for item in raw_html if item != '']
+    prep_html: list[str] = [item.strip() for item in raw_html if '<' in item and '>' in item]
 
     return prep_html
 
 
 html_content: list[str] = get_html()
+curr_line = 0
 
-for html_line in html_content:
-    if '<' not in html_line or '>' not in html_line:
-        continue
+while curr_line < 460:
+
+    curr_line += 1
+    html_line = html_content[curr_line]
 
     soup = BeautifulSoup(html_line, 'html.parser')
 
-    start = soup.find(name='strong')
+    if curr_line == 319:
+        continue
 
-    if start:
-        txt = start.next_sibling
-        if txt is not None:
-            print(txt)
+    elif curr_line < 40:
+        start = soup.find(name='strong')
+        if start:
+            txt = start.next_sibling
+            if txt is not None and clean_string(str(txt)) != '':
+                print(f'{curr_line} | {clean_words(str(start.text))} : {clean_string(str(txt))}')
 
-#
-# # start_words: element.ResultSet = soup.find_all(name='strong')
-# # other_words: element.ResultSet = soup.find_all(name='b')
-#
-
-# for item in start_words:
-#     print(type(item))
-
-#
-#
-#     if ' ' not in word:
-#         if word[8:-9].lower() not in all_words:
-#             all_words.append(word[8:-9].lower())
-#
-# for item in other_words:
-#     word = str(item)
-#
-#     if word == '<b>Яхонт</b>':
-#         all_words.append(word[3:-4].lower())
-#         break
-#
-#     elif ' ' not in word:
-#         slice_word = word[3:-4]
-#
-#         slice_word = slice_word.replace('<u>', '')
-#         slice_word = slice_word.replace('</u>', '')
-#         slice_word = slice_word.replace('&lt;', '')
-#         slice_word = slice_word.replace('&lt;', '')
-#         slice_word = slice_word.replace('&gt;', '')
-#         slice_word = slice_word.replace('!', '')
-#         slice_word = slice_word.replace('?', '')
-#
-#         if slice_word.lower() not in all_words:
-#             all_words.append(slice_word.lower())
-#
-# for _ in all_words:
-#     print(_)
+    elif curr_line > 40:
+        start = soup.find(name='b')
+        if start:
+            txt = start.next_sibling
+            if txt is not None and clean_string(str(txt)) != '':
+                print(f'{curr_line} | {clean_words(str(start.text))} : {clean_string(str(txt))}')
