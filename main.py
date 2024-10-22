3c10b0f832bd4f00e475cec814924d27b86a5465
Вадим Голов <grafit-design@inbox.ru>
2024-10-22 23:01:49 +0500

    Закончил с парсингом HTML, создал словарь слов и значений

--- main.py
+++ main.py
@@ -14,23 +14,20 @@ def clean_string(line: str) -> str:
 
     forbidden_words: list[str] = ['антилокус', 'Антилокус', 'локус', 'Локус', '()']
 
-    for item in forbidden_words:
-        text = text.replace(item, '')
+    for itm in forbidden_words:
+        text: str = text.replace(itm, '')
 
     closing_quote: int = text.find('»', 1)
     if closing_quote != -1:
         text: str = text[:closing_quote + 1]
 
-    text = text[0].upper() + text[1:]
+    text: str = text[0].upper() + text[1:]
 
     return text
 
 
 def clean_words(word: str) -> str:
-    clean = word.replace('«', '')
-    clean = word.replace('»', '')
-
-    clean = re.sub(r'[^а-яА-ЯёЁ]+$', '', word)
+    clean: str = re.sub(r'[^а-яА-ЯёЁ]+$', '', word)
 
     open_bracket: int = clean.find('(', 1)
     if open_bracket != -1:
@@ -44,46 +41,59 @@ def clean_words(word: str) -> str:
     if slash_pos != -1:
         clean: str = clean[:slash_pos]
 
-    clean = clean[0].upper() + clean[1:]
+    clean: str = clean[0].upper() + clean[1:]
 
-    clean = clean.strip()
+    clean: str = clean.strip()
 
     return clean
 
 
 def get_html() -> list[str]:
-    slovar_url: str = 'https://slovar.kakras.ru'
-    txt_html: str = requests.get(slovar_url).text
+    dictionary_url: str = 'https://slovar.kakras.ru'
+    txt_html: str = requests.get(dictionary_url).text
 
     raw_html: list[str] = txt_html.split('\r\n')
-    prep_html: list[str] = [item.strip() for item in raw_html if '<' in item and '>' in item]
+    prep_html: list[str] = [itm.strip() for itm in raw_html if '<' in itm and '>' in itm]
 
     return prep_html
 
 
+parsed_dictionary: dict[str, str] = {}
+
 html_content: list[str] = get_html()
-curr_line = 0
+curr_line: int = 0
 
 while curr_line < 460:
 
     curr_line += 1
-    html_line = html_content[curr_line]
+    html_line: str = html_content[curr_line]
 
-    soup = BeautifulSoup(html_line, 'html.parser')
+    soup: BeautifulSoup = BeautifulSoup(html_line, 'html.parser')
+    start: None = None
 
     if curr_line == 319:
         continue
 
     elif curr_line < 40:
-        start = soup.find(name='strong')
-        if start:
-            txt = start.next_sibling
-            if txt is not None and clean_string(str(txt)) != '':
-                print(f'{curr_line} | {clean_words(str(start.text))} : {clean_string(str(txt))}')
+        start: BeautifulSoup = soup.find(name='strong')
 
     elif curr_line > 40:
-        start = soup.find(name='b')
-        if start:
-            txt = start.next_sibling
-            if txt is not None and clean_string(str(txt)) != '':
-                print(f'{curr_line} | {clean_words(str(start.text))} : {clean_string(str(txt))}')
+        start: BeautifulSoup = soup.find(name='b')
+
+    if start:
+        meaning: str = ''
+
+        wrd: str = clean_words(str(start.text))
+        txt = start.next_sibling
+        if txt is not None:
+            meaning = clean_string(str(txt))
+
+        if wrd != '' and meaning != '':
+            parsed_dictionary[wrd] = meaning
+
+
+curr_line = 0
+
+for item, item_meaning in parsed_dictionary.items():
+    curr_line += 1
+    print(f'{curr_line} | {item} : {item_meaning}')
